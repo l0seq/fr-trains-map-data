b6cfd0f21ba27326225b49e57aba2c6b489398be
Loseq <loseq@proton.me>
2024-06-10 14:56:03 +0200

    Add mkdir in assets script

--- downloadAllAssets.sh
+++ downloadAllAssets.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p static/
 rm static/*
 wget -O static/intercites.json "https://transport.data.gouv.fr/resources/conversions/55940/GeoJSON"
 wget -O static/ter.zip "https://eu.ftp.opendatasoft.com/sncf/gtfs/export-ter-gtfs-last.zip"
